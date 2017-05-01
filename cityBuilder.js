98b2d1b4e3dbe95ca6c5a3360b022fe88c1019b7
Maxim <maxim_st@gmx.de>
2017-05-01 12:55:23 +0200

    Draw Tool

--- cityBuilder.js
+++ cityBuilder.js
@@ -1,55 +1,64 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var mouseX, mouseY;
-var lassoX1,lassoY1,lassoX2,lassoY2,isMouseDragging;
+var mouseX, mouseY, mouseClicked;
+var posX,posY;
+
+var gridSize = 1;
+
+var blocks = [];
+
+function block(x,y,size){
+	this.x = x;
+	this.y = y;
+	this.size = size;
+
+	this.draw = function(){
+		ctx.fillRect(this.x,this.y,this.size,this.size);
+	}
+}
 
 canvas.addEventListener('mousemove', function(evt) {
 	if (evt.offsetX) {
         mouseX = evt.offsetX;
         mouseY = evt.offsetY;
-    }else if (evt.layerX) {
-        mouseX = evt.layerX;
-        mouseY = evt.layerY;
     }
-	
-	if(isMouseDragging){
-		lassoX2 = mouseX;
-		lassoY2 = mouseY;
-		OutlineRect(lassoX1, lassoY1, lassoX2, lassoY2, 'red');
-	}
 });
 
 canvas.addEventListener('mousedown', function(evt){
 	if(evt.button === 0){
-		lassoX1 = mouseX;
-		lassoY1 = mouseY;
-		lassoX2 = lassoX1;
-		lassoY2 = lassoY1;
-		isMouseDragging = true;
+		blocks.push(new block(posX*gridSize,posY*gridSize,gridSize));
+		mouseClicked = true;
 	}
 });
 
 canvas.addEventListener('mouseup', function(evt){
 	if(evt.button === 0){
-		isMouseDragging = false;
+		mouseClicked = false;
 	}
 });
 
-function getAngleDeg(p1x,p1y,p2x,p2y){
-	return(Math.atan2(p2y - p1y, p2x - p1x) * 180 / Math.PI);
-}
+canvas.addEventListener('wheel', function(evt){
+	if(evt.deltaY > 0){
+		gridSize ++;
+	}else{
+		gridSize--;
+	}
+	console.log(gridSize);
+});
 
-function OutlineRect(corner1X, corner1Y, corner2X, corner2Y, lineColor) {
+function update(){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	ctx.save();
-    ctx.strokeStyle = lineColor;
-    ctx.beginPath();
-    ctx.rect(corner1X, corner1Y, corner2X-corner1X, corner2Y-corner1Y);
-    ctx.translate((corner2X-corner1X)/2,(corner2Y-corner1Y)/2);
-    ctx.rotate(getAngleDeg(corner1X,corner1Y,corner2X,corner2Y));
-    ctx.stroke();
-    ctx.restore();
+	posX = Math.floor(mouseX/gridSize);
+    posY = Math.floor(mouseY/gridSize);
+
+	if(mouseClicked){
+		blocks.push(new block(posX*gridSize,posY*gridSize,gridSize));
+	}
+	ctx.fillRect(posX*gridSize,posY*gridSize,gridSize,gridSize);
+	for(var i = 0; i < blocks.length; i++){
+		blocks[i].draw();
+	}
 }
 
-ctx.fillRect(10,5,10,10);
+setInterval(update,1000/60);
